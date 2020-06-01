2d9c5212a8fcba6a807925253ade403ac0ef0bab
p1ngu1n0 <36717227+p1ngu1n0@users.noreply.github.com>
2020-06-02 01:17:57 +0200

    hola class to box1.py

--- box1.py
+++ box1.py
@@ -1,8 +1,19 @@
 #!/usr/bin/env python3
 # coding=utf-8
 
+class hola(object):
+    """docstring for hola."""
+
+    def __init__(self):
+        super(hola, self).__init__()
+
+    def saludar(self, arg):
+        print("Hola ", arg)
+
+
 def main():
-    pass
+    hola.saludar(hola, "alcachofa")
+
 
 if __name__ == "__main__":
     main()
